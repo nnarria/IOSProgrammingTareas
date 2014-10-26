eb8518fdf28cc332f244d188d4948b718ed6597c
nnarria <nnarria@gmail.com>
2014-10-26 01:40:33 -0300

    ordering code
    minor changes ordering code

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
-## This function computes the inverse of a matrix storing the result in cache. 
+
+## This function computes the inverse of a matrix storing the result in cache.
 ## In this way future visits to this object if not re-calculate estimated
 
-## is used as an object which has an attribute to store the matrix and 
-## another to store the inverse matrix. Besides the methods defined to 
+## is used as an object which has an attribute to store the matrix and
+## another to store the inverse matrix. Besides the methods defined to
 ## access and modify attributes
 
 makeCacheMatrix <- function(x = matrix()) {
-	minv <- NULL
+				minv <- NULL
         set <- function(y) {
                 x <<- y
                 minv <<- NULL
@@ -21,12 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
              getminverse = getminverse)
 }
 
-## This function is responsible for managing whether the calculation 
+## This function is responsible for managing whether the calculation
 ## of the inverse matrix is stored or not.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-	minv <- x$getminverse()
+				minv <- x$getminverse()
         if(!is.null(minv)) {
                 message("getting cached data")
                 return(minv)
